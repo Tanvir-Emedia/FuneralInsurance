284e9f8ef6d11e58a13e8e1d2fc612b59862bcbc
Tanvir-Emedia <tanvir.emedia@outlook.com>
2016-04-13 18:29:20 +0530

    first push

---
+++ FuneralInsurance/Controllers/FuneralController.cs
@@ -0,0 +1,15 @@
+﻿using FuneralService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FuneralInsurance.Controllers
+{
+    public class FuneralController : ApiController
+    {
+        
+    }
+}
---
+++ FuneralInsurance/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FuneralInsurance.WebApiApplication" Language="C#" %>
